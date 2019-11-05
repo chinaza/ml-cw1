973d105497543d0e347be3607345e40a43c9a69a
Alex Johnson <alextheviking@hotmail.co.uk>
2019-11-05 10:16:45 +0000

    Adds working linear regression code and removes old files

--- classification.py
+++
@@ -1,7 +0,0 @@
-import scipy.io as sio # The library to deal with .mat
-
-matstruct_squeezed = sio.loadmat('facialPoints.mat', squeeze_me=True)
-points=matstruct_squeezed['points']
-
-matstruct_squeezed = sio.loadmat('labels.mat', squeeze_me=True)
-labels=matstruct_squeezed['labels']
--- tf.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import tensorflow as tf
-
-X_1 = tf.placeholder(tf.float32, name = "X_1")
-X_2 = tf.placeholder(tf.float32, name = "X_2")
-Y = tf.add(X_1, X_2, name = "Y")
-with tf.Session() as session:
- result = session.run(Y, feed_dict={X_1:[1,2,3], X_2:[4,5,6]})
- print(result)
